ea946a30a9ad5c5b534a67b4e47f801921f4cf34
Melan Rashitha Dias <30628058+melan96@users.noreply.github.com>
2018-07-09 09:09:43 -0700

    Stack Class Added Push call

---
+++ StackX.java
@@ -0,0 +1,18 @@
+ public class StackX{
+
+
+	private int topValue;
+	private double[] stackArray;
+	private int stackSize;
+
+
+	//initialize the constructor
+	public StackX(int stackSize){
+		
+		stackArray[] = new double[stackSize];
+		topValue=-1;
+		
+		
+	}
+
+}
